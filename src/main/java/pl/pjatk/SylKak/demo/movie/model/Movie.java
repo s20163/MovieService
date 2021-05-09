2af17af8a246616caeae25a4f4232ac0124790d0
s20163 <s20163@pjwstk.edu.pl>
2021-05-09 15:57:13 +0200

    Update Movie.java

--- src/main/java/pl/pjatk/SylKak/demo/movie/model/Movie.java
+++ src/main/java/pl/pjatk/SylKak/demo/movie/model/Movie.java
@@ -12,4 +12,36 @@ public class Movie {
         this.genre = genre;
         this.durationInMin = durationInMin;
     }
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public MovieGenre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(MovieGenre genre) {
+        this.genre = genre;
+    }
+
+    public int getDurationInMin() {
+        return durationInMin;
+    }
+
+    public void setDurationInMin(int durationInMin) {
+        this.durationInMin = durationInMin;
+    }
 }
